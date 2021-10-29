94948db243a80993243a5a19128c13e66bbbf97c
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-29 10:08:17 +0530

    Adding First Name

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace DAY_19_UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Regex Problem");
+            Console.WriteLine("Welcome to user registration!");
+
+            string FirstName;
+            Console.Write("Enter First Name: ");
+            FirstName = Console.ReadLine();
+
+            UserRegistration.checkFirstName(FirstName);
+
+
+
+
         }
     }
 }
-
---
+++ UserRegistration.cs
@@ -0,0 +1,33 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DAY_19_UserRegistration
+{
+    public class UserRegistration
+    {
+        public static string Regex_FirstName = "^[A-Z][a-z]{2,}$";
+
+        public static bool ValidateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, Regex_FirstName);
+        }
+
+
+        public static void checkFirstName(string FirstName)
+        {
+            if (Regex.IsMatch(FirstName, Regex_FirstName) == false)
+            {
+                Console.WriteLine("Enter valid First name. It should Start with capital letters and minimum 3 letters!");
+            }
+            else
+            {
+                Console.WriteLine("It is Valid!");
+            }
+        }
+
+    }
+}
